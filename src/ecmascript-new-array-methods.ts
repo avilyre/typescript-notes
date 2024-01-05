030dd9615f074bb1deda0dad235c1732e7cabb56
Avily Silva <avilyre@gmail.com>
2024-01-05 11:42:30 -0300

    New array method Array.toSpliced() from ECMA2023

--- src/ecmascript-new-array-methods.ts
+++ src/ecmascript-new-array-methods.ts
@@ -13,3 +13,11 @@ console.log("Array.toSorted():", result);
 const numbers2 = [5, 1, 4, 3, 2];
 const result2 = numbers.toReversed();
 console.log("Array.toReversed():", result2);
+
+/**
+ * Method: Array.toSpliced();
+ * The original array it's not modified and return a new array with it's value modified
+ */
+const numbers3 = [5, 1, 4, 3, 2];
+const result3 = numbers.toSpliced(0, 2);
+console.log("Array.toSpliced():", result3);
