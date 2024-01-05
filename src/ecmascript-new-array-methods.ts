eb84c3ae7d59ba7465b8ca547de7d498086ef14f
Avily Silva <avilyre@gmail.com>
2024-01-05 11:39:34 -0300

    New array method Array.toReversed() from ECMA2023

--- src/ecmascript-new-array-methods.ts
+++ src/ecmascript-new-array-methods.ts
@@ -4,4 +4,12 @@
  */
 const numbers = [5, 1, 4, 3, 2];
 const result = numbers.toSorted();
-console.log(result);
+console.log("Array.toSorted():", result);
+
+/**
+ * Method: Array.toReversed();
+ * The original array it's not modified and return a new array with it's value modified
+ */
+const numbers2 = [5, 1, 4, 3, 2];
+const result2 = numbers.toReversed();
+console.log("Array.toReversed():", result2);
