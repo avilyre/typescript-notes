468480c42159e315ca5ab6ca1c493b39f9c2125b
Avily Silva <avilyre@gmail.com>
2024-01-05 11:37:13 -0300

    New array method Array.toSorted() from ECMA2023

---
+++ src/ecmascript-new-array-methods.ts
@@ -0,0 +1,7 @@
+/**
+ * Method: Array.toSorted();
+ * The original array it's not modified and return a new array with it's value modified
+ */
+const numbers = [5, 1, 4, 3, 2];
+const result = numbers.toSorted();
+console.log(result);
